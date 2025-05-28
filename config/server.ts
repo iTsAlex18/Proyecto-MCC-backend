688498570ef9d8fefe48da5dd6e59aa991254dc6
iTsAlex18 <jesus.bracamontediaz@cananea.tecnm.mx>
2025-05-28 11:38:16 -0700

    fix: server.ts

--- config/server.ts
+++ config/server.ts
@@ -1,5 +1,5 @@
 module.exports = ({ env }) => ({
-  url: env('PUBLIC_URL', 'http://localhost:1337'), // 👈 esta línea es clave
+  url: env('PUBLIC_URL', 'https://proyecto-mcc-backend.onrender.com'), // 👈 esta línea es clave
   host: '0.0.0.0',
   port: env.int('PORT', 1337),
   app: {
