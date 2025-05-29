a8552e6858954975ef358b546653d2ef09c9310b
iTsAlex18 <jesus.bracamontediaz@cananea.tecnm.mx>
2025-05-29 15:49:57 -0700

    cloudinary

--- config/plugins.ts
+++ config/plugins.ts
@@ -4,6 +4,14 @@ export default ({ env }) => ({
       jwtSecret: env('JWT_SECRET'),
     },
   },
-
-  // Plugin eliminado correctamente
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+    },
+  },
 });
