cbcb06da197d8acf829880fae3af06cbdc67697e
iTsAlex18 <jesus.bracamontediaz@cananea.tecnm.mx>
2025-05-29 10:26:49 -0700

    fix: cors

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,16 +2,7 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  {
-    name: 'strapi::cors',
-    config: {
-      enabled: true,
-      origin: [
-        'https://proyecto-hj0c2l8z8-itsalex18s-projects.vercel.app', // front-end Vercel
-      ],
-      credentials: true,
-    },
-  },
+  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
