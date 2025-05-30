f8fea0975994f07d3437c6282a7defef233d39d6
iTsAlex18 <jesus.bracamontediaz@cananea.tecnm.mx>
2025-05-29 18:10:43 -0700

    fix: img cloud

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,9 +1,32 @@
-export default [
-  'strapi::logger',
+module.exports = [
   'strapi::errors',
   'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://res.cloudinary.com',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://res.cloudinary.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
